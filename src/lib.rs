eb9a274a8d538b0569e79ef0ced08fc5649da883
Jakub Arnold <darthdeus@gmail.com>
2023-10-07 17:53:53 +0200

    GAT ID almost works?

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,50 @@ use std::{
 };
 
 use thunderdome::Arena;
+use paste::paste;
+
+pub trait Component: 'static + Sized {
+    type Id: Copy;
+}
+
+// macro_rules! component {
+//     ($ty:ident) => {
+//         #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+//         paste! {
+//             pub struct [< $ty Id >](thunderdome::Index);
+//         }
+//
+//
+//         impl Component for $ty {
+//             type Id = paste! [< $ty Id >];
+//         }
+//     };
+// }
+
+macro_rules! component {
+    ($ty:ident) => {
+        paste! {
+            #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+            pub struct  [<$ty Id>] (thunderdome::Index);
+        }
+
+        impl Component for $ty {
+            type Id = paste! { [<$ty Id>] };
+        }
+    };
+}
+
+// macro_rules! component {
+//     ($ty:ident, $id:ident) => {
+//         #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+//         pub struct $id(thunderdome::Index);
+//
+//
+//         impl Component for $ty {
+//             type Id = $id;
+//         }
+//     };
+// }
 
 pub struct Store {
     pub data: HashMap<TypeId, Box<dyn Any>>,
@@ -16,7 +60,7 @@ impl Store {
         }
     }
 
-    pub fn spawn<T: 'static>(&mut self, value: T) {
+    pub fn spawn<T: Component>(&mut self, value: T) {
         let type_id = TypeId::of::<T>();
 
         if let Some(arena) = self.data.get_mut(&type_id) {
@@ -28,7 +72,7 @@ impl Store {
         }
     }
 
-    pub fn iter<T: 'static>(&self) -> Box<dyn Iterator<Item = &T> + '_> {
+    pub fn iter<T: Component>(&self) -> Box<dyn Iterator<Item = &T> + '_> {
         let type_id = TypeId::of::<T>();
 
         if let Some(arena) = self.data.get(&type_id) {
@@ -45,18 +89,34 @@ impl Store {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Thing {
+    pub x: i32,
+}
+
+component!(Thing);
+// impl Component for Thing {
+//     type Id = ThingId;
+// }
+//
+// #[derive(Copy, Clone)]
+// struct ThingId(thunderdome::Index);
+
 #[cfg(test)]
 mod tests {
-    use crate::Store;
+    use crate::{Component, Store, Thing};
+
+    // #[test]
+    // fn empty_store_is_okay() {
+    //     let store = Store::new();
+    //
+    //     assert_eq!(None, store.iter::<i32>().next());
+    //     assert_eq!(None, store.iter::<f32>().next());
+    // }
 
     #[test]
     fn simple_test() {
         let mut store = Store::new();
-
-        #[derive(Debug, PartialEq)]
-        struct Thing {
-            pub x: i32,
-        }
 
         store.spawn(Thing { x: 1 });
         store.spawn(Thing { x: 2 });
