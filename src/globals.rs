0337a19797bb04b475b3b64ed25a2c523ae7a151
kampffrosch94 <kampffrosch94@github.com>
2023-10-08 10:40:24 +0200

    something is working

--- src/globals.rs
+++ src/globals.rs
@@ -5,7 +5,7 @@ use once_cell::sync::Lazy;
 #[allow(unused_imports)]
 use atomic_refcell::{AtomicRef, AtomicRefCell};
 
-pub static STORE: Lazy<AtomicRefCell<Store>> = Lazy::new(|| AtomicRefCell::new(Store::new()));
+//pub static STORE: Lazy<AtomicRefCell<Store>> = Lazy::new(|| AtomicRefCell::new(Store::new()));
 
 // Here lies sadness
 // pub fn iter<T>() -> AtomicRef<'static, &'static Box<dyn Iterator<Item = (Id<T>, &'static T)>>> {
@@ -16,22 +16,46 @@ pub static STORE: Lazy<AtomicRefCell<Store>> = Lazy::new(|| AtomicRefCell::new(S
 
 // Alternative sadness, no less sad
 
-// use parking_lot::Mutex;
+use parking_lot::*;
 //
-// type Store = Vec<i32>;
 //
-// pub static STORE: Lazy<Mutex<Store>> = Lazy::new(|| Mutex::new(Store::new()));
-//
-// pub fn iter<T>() -> MutexGuard<'static, Box<dyn Iterator<Item = &T>>> {
-//     MutexGuard::map(STORE.lock(), |store| &store.iter::<T>())
-// }
-//
-// use parking_lot::*;
-//
-// type Store = Vec<i32>;
-//
-// pub static STORE: Lazy<Mutex<Store>> = Lazy::new(|| Mutex::new(Store::new()));
-//
-// pub fn iter<'a>() -> MappedMutexGuard<'static, impl Iterator<Item = &'a i32>> {
-//     MutexGuard::map(STORE.lock(), |store| &mut store.iter())
-// }
+type Store = Vec<i32>;
+type Guard = MutexGuard<'static, Store>;
+
+pub static STORE: Lazy<Mutex<Store>> = Lazy::new(|| Mutex::new(Store::new()));
+
+pub fn iter<T>() -> StoreHolder<T> {
+    //MutexGuard::map(STORE.lock(), |store| &mut store[0])
+    StoreHolder { guard: STORE.lock(), phantom_data: PhantomData }
+}
+
+pub struct StoreHolder<T: 'static> {
+    guard: Guard,
+    phantom_data: PhantomData<T>,
+}
+
+impl<'a, T> IntoIterator for &'a StoreHolder<T> {
+    type Item = &'a i32;
+    type IntoIter = StoreIterator<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        StoreIterator {
+            holder: self,
+            current: 0,
+            phantom_data: PhantomData,
+        }
+    }
+}
+
+pub struct StoreIterator<'a, T: 'static> {
+    holder: &'a StoreHolder<T>,
+    current: usize,
+    phantom_data: PhantomData<&'a T>,
+}
+
+impl<'a, T> Iterator for StoreIterator<'a, T> {
+    type Item = &'a i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.holder.guard.get(self.current)
+    }
+}
