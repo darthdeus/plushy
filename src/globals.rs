86ace6f40ee454cd0339771a7607ddc6d07d60e3
kampffrosch94 <kampffrosch94@github.com>
2023-10-08 10:59:04 +0200

    this could work

--- src/globals.rs
+++ src/globals.rs
@@ -17,45 +17,47 @@ use atomic_refcell::{AtomicRef, AtomicRefCell};
 // Alternative sadness, no less sad
 
 use parking_lot::*;
-//
-//
-type Store = Vec<i32>;
-type Guard = MutexGuard<'static, Store>;
+//type Store = Vec<i32>;
+type Guard<T> = MappedMutexGuard<'static, Arena<T>>;
 
 pub static STORE: Lazy<Mutex<Store>> = Lazy::new(|| Mutex::new(Store::new()));
 
 pub fn iter<T>() -> StoreHolder<T> {
-    //MutexGuard::map(STORE.lock(), |store| &mut store[0])
-    StoreHolder { guard: STORE.lock(), phantom_data: PhantomData }
+    let type_id = TypeId::of::<T>();
+    let guard = MutexGuard::map(STORE.lock(), |store| {
+        store
+            .data
+            .get_mut(&type_id)
+            .unwrap() // TODO create if it doesn't exist, entry api?
+            .downcast_mut::<Arena<T>>()
+            .unwrap()
+    });
+    StoreHolder { guard }
 }
 
 pub struct StoreHolder<T: 'static> {
-    guard: Guard,
-    phantom_data: PhantomData<T>,
+    guard: Guard<T>,
 }
 
 impl<'a, T> IntoIterator for &'a StoreHolder<T> {
-    type Item = &'a i32;
+    type Item = (Id<T>, &'a T);
     type IntoIter = StoreIterator<'a, T>;
     fn into_iter(self) -> Self::IntoIter {
         StoreIterator {
-            holder: self,
-            current: 0,
-            phantom_data: PhantomData,
+            holder: self.guard.iter(),
         }
     }
 }
 
 pub struct StoreIterator<'a, T: 'static> {
-    holder: &'a StoreHolder<T>,
-    current: usize,
-    phantom_data: PhantomData<&'a T>,
+    holder: thunderdome::iter::Iter<'a, T>,
 }
 
 impl<'a, T> Iterator for StoreIterator<'a, T> {
-    type Item = &'a i32;
+    type Item = (Id<T>, &'a T);
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.holder.guard.get(self.current)
+        //self.holder.guard.get(self.current)
+        self.holder.next().map(|x| (Id(x.0, PhantomData), x.1))
     }
 }
