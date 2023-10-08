c7c36b35c71e799ff9aa8d88fecbde5bc4fd92e8
kampffrosch94 <kampffrosch94@github.com>
2023-10-08 11:04:28 +0200

    test looks awkward

--- src/globals.rs
+++ src/globals.rs
@@ -1,23 +1,8 @@
 use crate::*;
 
 use once_cell::sync::Lazy;
-
-#[allow(unused_imports)]
-use atomic_refcell::{AtomicRef, AtomicRefCell};
-
-//pub static STORE: Lazy<AtomicRefCell<Store>> = Lazy::new(|| AtomicRefCell::new(Store::new()));
-
-// Here lies sadness
-// pub fn iter<T>() -> AtomicRef<'static, &'static Box<dyn Iterator<Item = (Id<T>, &'static T)>>> {
-//     AtomicRef::map(STORE.borrow(), |store| &store.iter::<T>())
-// }
-
-// ****************************************
-
-// Alternative sadness, no less sad
-
 use parking_lot::*;
-//type Store = Vec<i32>;
+
 type Guard<T> = MappedMutexGuard<'static, Arena<T>>;
 
 pub static STORE: Lazy<Mutex<Store>> = Lazy::new(|| Mutex::new(Store::new()));
@@ -39,25 +24,40 @@ pub struct StoreHolder<T: 'static> {
     guard: Guard<T>,
 }
 
-impl<'a, T> IntoIterator for &'a StoreHolder<T> {
-    type Item = (Id<T>, &'a T);
+impl<'a, T> IntoIterator for &'a mut StoreHolder<T> {
+    type Item = (Id<T>, &'a mut T);
     type IntoIter = StoreIterator<'a, T>;
     fn into_iter(self) -> Self::IntoIter {
         StoreIterator {
-            holder: self.guard.iter(),
+            holder: self.guard.iter_mut(),
         }
     }
 }
 
 pub struct StoreIterator<'a, T: 'static> {
-    holder: thunderdome::iter::Iter<'a, T>,
+    holder: thunderdome::iter::IterMut<'a, T>,
 }
 
 impl<'a, T> Iterator for StoreIterator<'a, T> {
-    type Item = (Id<T>, &'a T);
+    type Item = (Id<T>, &'a mut T);
 
     fn next(&mut self) -> Option<Self::Item> {
         //self.holder.guard.get(self.current)
         self.holder.next().map(|x| (Id(x.0, PhantomData), x.1))
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mut_iter() {
+        let val: i32 = 1;
+        STORE.lock().spawn(val);
+        for val in iter::<i32>().into_iter() {
+            assert_eq!(1, *val.1);
+        }
     }
 }
